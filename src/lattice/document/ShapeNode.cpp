bb8e8440895403b0065f7be9577d60277037479c
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-17 17:03:20 +0900

    Make ShapeNode abstract

--- src/lattice/document/ShapeNode.cpp
+++ src/lattice/document/ShapeNode.cpp
@@ -13,10 +13,6 @@ bool ShapeNode::canInsertNode(const SP<Node> &node) const {
     return !!std::dynamic_pointer_cast<ShapeNode>(node);
 }
 
-SP<Node> ShapeNode::newInstance(const SP<History> &history) const {
-    return std::make_shared<ShapeNode>(history);
-}
-
 QString ShapeNode::type() const {
     return "shape";
 }
--- src/lattice/document/ShapeNode.hpp
+++ src/lattice/document/ShapeNode.hpp
@@ -1,17 +1,19 @@
 #pragma once
 #include "Node.hpp"
+#include <TopoDS_Shape.hxx>
 
 namespace lattice {
 
-class ShapeNode final : public Node {
+class ShapeNode : public Node {
     Q_OBJECT
   public:
     ShapeNode(const SP<History> &history);
 
     bool canHaveChildren() const override;
     bool canInsertNode(const SP<Node> &node) const override;
-    SP<Node> newInstance(const SP<History> &history) const override;
     QString type() const override;
+
+    virtual TopoDS_Shape shape() const = 0;
 };
 
 } // namespace lattice
