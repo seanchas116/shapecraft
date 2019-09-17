0ef2d9223d5b3276c23ba1ef2a6513fe0dec1deb
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-17 16:44:24 +0900

    Implement ShapeNode virtual functions

--- src/lattice/document/ShapeNode.cpp
+++ src/lattice/document/ShapeNode.cpp
@@ -5,4 +5,20 @@ namespace lattice {
 ShapeNode::ShapeNode(const SP<History> &history) : Node(history) {
 }
 
+bool ShapeNode::canHaveChildren() const {
+    return true;
+}
+
+bool ShapeNode::canInsertNode(const SP<Node> &node) const {
+    return !!std::dynamic_pointer_cast<ShapeNode>(node);
+}
+
+SP<Node> ShapeNode::newInstance(const SP<History> &history) const {
+    return std::make_shared<ShapeNode>(history);
+}
+
+QString ShapeNode::type() const {
+    return "shape";
+}
+
 } // namespace lattice
--- src/lattice/document/ShapeNode.hpp
+++ src/lattice/document/ShapeNode.hpp
@@ -3,10 +3,15 @@
 
 namespace lattice {
 
-class ShapeNode : public Node {
+class ShapeNode final : public Node {
     Q_OBJECT
   public:
     ShapeNode(const SP<History> &history);
+
+    bool canHaveChildren() const override;
+    bool canInsertNode(const SP<Node> &node) const override;
+    SP<Node> newInstance(const SP<History> &history) const override;
+    QString type() const override;
 };
 
 } // namespace lattice
