6b578c110a6d87bcf7341c5cb19636f4c998efdd
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-18 23:18:26 +0900

    Rename

--- src/shapecraft/app/editor/CameraState.hpp
+++ src/shapecraft/app/editor/CameraState.hpp
@@ -36,14 +36,14 @@ class CameraState : public QObject {
   private:
     static glm::dvec3 orientationAngle(Orientation orientation);
 
-    LATTICE_AUTO_PROPERTY_REFERENCE(Camera::Projection, projection, setProjection, Camera::Projection::Perspective)
-    LATTICE_AUTO_PROPERTY(glm::dvec3, position, setPosition, glm::dvec3(0, 10, 10))
-    LATTICE_AUTO_PROPERTY(glm::dvec3, eulerAngles, setEulerAngles, glm::dvec3(0))
-    LATTICE_AUTO_PROPERTY(glm::dvec2, viewportSize, setViewportSize, glm::dvec2(100, 100))
-    LATTICE_AUTO_PROPERTY(double, fieldOfView, setFieldOfView, glm::radians(60.0))
-    LATTICE_AUTO_PROPERTY(double, zNear, setZNear, 0.1)
-    LATTICE_AUTO_PROPERTY(double, zFar, setZFar, 100.0)
-    LATTICE_AUTO_PROPERTY(double, orthoScale, setOrthoScale, 100.0)
+    SHAPECRAFT_PROPERTY(Camera::Projection, projection, setProjection, Camera::Projection::Perspective)
+    SHAPECRAFT_PROPERTY(glm::dvec3, position, setPosition, glm::dvec3(0, 10, 10))
+    SHAPECRAFT_PROPERTY(glm::dvec3, eulerAngles, setEulerAngles, glm::dvec3(0))
+    SHAPECRAFT_PROPERTY(glm::dvec2, viewportSize, setViewportSize, glm::dvec2(100, 100))
+    SHAPECRAFT_PROPERTY(double, fieldOfView, setFieldOfView, glm::radians(60.0))
+    SHAPECRAFT_PROPERTY(double, zNear, setZNear, 0.1)
+    SHAPECRAFT_PROPERTY(double, zFar, setZFar, 100.0)
+    SHAPECRAFT_PROPERTY(double, orthoScale, setOrthoScale, 100.0)
 
     void emitCameraChanged();
 };
