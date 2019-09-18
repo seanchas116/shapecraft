27986d03f123994a2316e169b69de4745aa9f354
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-18 21:27:10 +0900

    Setup surface

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,27 @@
 #include "shapecraft/app/view/MainWindow.hpp"
 #include <QApplication>
+#include <QSurfaceFormat>
 
 int main(int argc, char *argv[]) {
     using namespace shapecraft;
 
+    QApplication::setAttribute(Qt::AA_UseDesktopOpenGL);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+
     QApplication app(argc, argv);
+
+    QSurfaceFormat glFormat;
+    glFormat.setVersion(3, 3);
+    glFormat.setProfile(QSurfaceFormat::CoreProfile);
+    glFormat.setDepthBufferSize(24);
+    glFormat.setSamples(4);
+
+#ifdef QT_DEBUG
+    glFormat.setOption(QSurfaceFormat::DebugContext);
+#endif
+
+    QSurfaceFormat::setDefaultFormat(glFormat);
 
     auto window = new MainWindow();
     window->show();
