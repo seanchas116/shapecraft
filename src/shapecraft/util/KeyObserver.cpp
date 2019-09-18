8975fbf31c043ce91faa198f2b700f95fec6a978
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-18 22:55:20 +0900

    Add KeyObserver

---
+++ src/shapecraft/util/KeyObserver.cpp
@@ -0,0 +1,44 @@
+#include "KeyObserver.hpp"
+#include <QApplication>
+#include <QKeyEvent>
+#include <QWindow>
+#include <QtDebug>
+
+namespace shapecraft {
+
+KeyObserver::KeyObserver() {
+    qApp->installEventFilter(this);
+}
+
+KeyObserver::~KeyObserver() {
+    qApp->removeEventFilter(this);
+}
+
+bool KeyObserver::eventFilter(QObject *watched, QEvent *event) {
+    if (!qobject_cast<QWindow *>(watched)) {
+        return false;
+    }
+    switch (event->type()) {
+    case QEvent::KeyPress:
+        keyPress(static_cast<QKeyEvent *>(event));
+        break;
+    case QEvent::KeyRelease:
+        keyRelease(static_cast<QKeyEvent *>(event));
+        break;
+    default:
+        break;
+    }
+    return false;
+}
+
+void KeyObserver::keyPress(QKeyEvent *event) {
+    _pressedKeys.insert(event->key());
+    emit pressedKeysChanged(_pressedKeys);
+}
+
+void KeyObserver::keyRelease(QKeyEvent *event) {
+    _pressedKeys.erase(event->key());
+    emit pressedKeysChanged(_pressedKeys);
+}
+
+} // namespace shapecraft
---
+++ src/shapecraft/util/KeyObserver.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <QObject>
+#include <unordered_set>
+
+class QKeyEvent;
+
+namespace shapecraft {
+
+class KeyObserver final : public QObject {
+    Q_OBJECT
+  public:
+    KeyObserver();
+    ~KeyObserver() override;
+
+    bool eventFilter(QObject *watched, QEvent *event) override;
+    void keyPress(QKeyEvent *event);
+    void keyRelease(QKeyEvent *event);
+
+    const auto &pressedKeys() const { return _pressedKeys; }
+
+  signals:
+    void pressedKeysChanged(const std::unordered_set<int> &keys);
+
+  private:
+    std::unordered_set<int> _pressedKeys;
+};
+
+} // namespace shapecraft
