2408070caf68601ec114e66d53a050e70315ea38
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-18 21:27:17 +0900

    Use EditorViewportContainer

--- src/shapecraft/app/view/MainWindow.cpp
+++ src/shapecraft/app/view/MainWindow.cpp
@@ -1,9 +1,13 @@
 #include "MainWindow.hpp"
+#include "shapecraft/app/editor/EditorViewportContainer.hpp"
 
 namespace shapecraft {
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
     setAttribute(Qt::WA_DeleteOnClose);
+
+    auto viewportContainer = new EditorViewportContainer();
+    setCentralWidget(viewportContainer);
 }
 
 } // namespace shapecraft
