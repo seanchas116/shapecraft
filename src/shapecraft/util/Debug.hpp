802e952fb6f426b8de6481fe3a5c2a9093ba4f25
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-23 02:12:55 +0900

    Add glm qDebug support

--- src/shapecraft/util/Debug.hpp
+++ src/shapecraft/util/Debug.hpp
@@ -4,9 +4,25 @@
 #include <QDebug>
 #include <QtDebug>
 #include <array>
+#include <glm/glm.hpp>
 #include <optional>
 
 namespace shapecraft {
+
+template <glm::length_t L, typename T, glm::qualifier Q>
+QDebug operator<<(QDebug debug, const glm::vec<L, T, Q> &v) {
+    QDebugStateSaver saver(debug);
+    debug.nospace();
+    debug << "(";
+    for (size_t i = 0; i < L; ++i) {
+        if (i != 0) {
+            debug << ", ";
+        }
+        debug << v[i];
+    }
+    debug << ")";
+    return debug;
+}
 
 template <typename T, size_t N>
 QDebug operator<<(QDebug debug, const std::array<T, N> &array) {
