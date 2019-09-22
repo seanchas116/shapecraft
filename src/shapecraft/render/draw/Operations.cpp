40f4029eecb5c45cd594bc3a5c4eaf85bbc77138
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-22 22:45:26 +0900

    Move copy to Operations

--- src/shapecraft/render/draw/Operations.cpp
+++ src/shapecraft/render/draw/Operations.cpp
@@ -1,9 +1,35 @@
 #include "Operations.hpp"
+#include "Constants.hpp"
+#include "shapecraft/Resource.hpp"
+#include "shapecraft/render/gl/Texture.hpp"
+#include "shapecraft/render/gl/VertexArray.hpp"
 
 namespace shapecraft {
 namespace draw {
 
-Operations::Operations() {
+namespace {
+
+struct CopyVAOAttribute {
+    glm::vec2 texCoord;
+};
+
+SP<gl::VertexArray> createCopyVAO() {
+    std::vector<CopyVAOAttribute> attributes = {
+        {glm::vec2(0, 0)},
+        {glm::vec2(1, 0)},
+        {glm::vec2(1, 1)},
+        {glm::vec2(0, 1)},
+    };
+
+    auto vbo = std::make_shared<gl::VertexBuffer<CopyVAOAttribute>>(attributes);
+    return std::make_shared<gl::VertexArray>(vbo, gl::Primitive::TriangleFan);
+}
+
+} // namespace
+
+Operations::Operations()
+    : _copyShader(resource::read(shaderDir + "Copy.vert"), {}, resource::read(shaderDir + "Copy.frag")),
+      _copyVAO(createCopyVAO()) {
     initializeOpenGLFunctions();
 }
 
@@ -16,6 +42,20 @@ void Operations::clear(glm::vec4 color, float depth) {
 void Operations::clearDepth(float depth) {
     glClearDepthf(depth);
     glClear(GL_DEPTH_BUFFER_BIT);
+}
+
+void Operations::copy(const SP<gl::Texture> &texture, const SP<gl::Texture> &depthTexture, float opacity) {
+    _copyShader.bind();
+
+    glActiveTexture(GL_TEXTURE0);
+    texture->bind();
+    glActiveTexture(GL_TEXTURE1);
+    depthTexture->bind();
+
+    _copyShader.setUniform("colorSampler", 0);
+    _copyShader.setUniform("depthSampler", 1);
+    _copyShader.setUniform("opacity", opacity);
+    _copyVAO->draw();
 }
 
 } // namespace draw
--- src/shapecraft/render/draw/Operations.hpp
+++ src/shapecraft/render/draw/Operations.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../gl/Shader.hpp"
 #include "./Clear.hpp"
 #include "./Copy.hpp"
 #include "./DrawCircle.hpp"
@@ -9,6 +10,12 @@
 #include <QOpenGLExtraFunctions>
 
 namespace shapecraft {
+
+namespace gl {
+class Texture;
+class VertexArray;
+} // namespace gl
+
 namespace draw {
 
 class Operations final : protected QOpenGLExtraFunctions {
@@ -18,11 +25,16 @@ class Operations final : protected QOpenGLExtraFunctions {
     void clear(glm::vec4 color, float depth);
     void clearDepth(float depth);
 
-    Copy copy;
+    void copy(const SP<gl::Texture> &texture, const SP<gl::Texture> &depthTexture, float opacity = 1);
+
     DrawCircle drawCircle;
     DrawLine drawLine;
     DrawMaterial drawMaterial;
     DrawUnicolor drawUnicolor;
+
+  private:
+    gl::Shader _copyShader;
+    SP<gl::VertexArray> _copyVAO;
 };
 
 } // namespace draw
