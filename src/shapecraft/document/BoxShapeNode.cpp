5f2ca9a8b8c652809f9af201390cc196b3ef3034
Ryohei Ikegami <iofg2100@gmail.com>
2019-09-29 16:35:01 +0900

    Implement BoxShapeNode

--- src/shapecraft/document/BoxShapeNode.cpp
+++ src/shapecraft/document/BoxShapeNode.cpp
@@ -1,8 +1,20 @@
 #include "BoxShapeNode.hpp"
+#include <BRepPrimAPI_MakeBox.hxx>
+#include <QTimer>
 
 namespace shapecraft {
 
 BoxShapeNode::BoxShapeNode(const SP<History> &history) : ShapeNode(history) {
+    connect(this, &ShapeNode::boundingBoxChanged, this, [this](const Box<double> &boundingBox) {
+        if (boundingBox.size().x > 0 && boundingBox.size().y > 0 && boundingBox.size().z > 0) {
+            gp_Pnt p0(boundingBox.minPosition().x, boundingBox.minPosition().y, boundingBox.minPosition().z);
+            gp_Pnt p1(boundingBox.maxPosition().x, boundingBox.maxPosition().y, boundingBox.maxPosition().z);
+            setShape(BRepPrimAPI_MakeBox(p0, p1).Shape());
+        }
+    });
+    QTimer::singleShot(0, this, [this] {
+        setBoundingBox(Box<double>::fromSize(glm::dvec3(0), glm::dvec3(1, 1, 1)));
+    });
 }
 
 SP<Node> BoxShapeNode::newInstance(const SP<History> &history) const {
