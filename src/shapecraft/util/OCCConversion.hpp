82f0af049fe5ce514e9af8831d19b6c9dc77c670
Ryohei Ikegami <iofg2100@gmail.com>
2019-10-05 16:28:16 +0900

    Use double in OCCConversion

--- src/shapecraft/util/OCCConversion.hpp
+++ src/shapecraft/util/OCCConversion.hpp
@@ -6,15 +6,14 @@
 
 namespace shapecraft {
 
-inline glm::mat4 toMat4(const gp_Trsf &t) {
-    return glm::mat4(t.Value(1, 1), t.Value(2, 1), t.Value(3, 1), 0,
-                     t.Value(1, 2), t.Value(2, 2), t.Value(3, 2), 0,
-                     t.Value(1, 3), t.Value(2, 3), t.Value(3, 3), 0,
-                     t.Value(1, 4), t.Value(2, 4), t.Value(3, 4), 1);
+inline glm::dmat4 toMat4(const gp_Trsf &t) {
+    return glm::dmat4(t.Value(1, 1), t.Value(2, 1), t.Value(3, 1), 0,
+                      t.Value(1, 2), t.Value(2, 2), t.Value(3, 2), 0,
+                      t.Value(1, 3), t.Value(2, 3), t.Value(3, 3), 0,
+                      t.Value(1, 4), t.Value(2, 4), t.Value(3, 4), 1);
 }
 
-inline gp_Trsf toTrsf(const glm::mat4 &fmat) {
-    glm::dmat4 t = fmat;
+inline gp_Trsf toTrsf(const glm::dmat4 &t) {
     gp_Trsf ret;
     ret.SetValues(t[0][0], t[1][0], t[2][0], t[3][0],
                   t[0][1], t[1][1], t[2][1], t[3][1],
@@ -22,12 +21,12 @@ inline gp_Trsf toTrsf(const glm::mat4 &fmat) {
     return ret;
 }
 
-inline glm::vec3 toVec3(const gp_Pnt &p) {
-    return glm::vec3(p.X(), p.Y(), p.Z());
+inline glm::dvec3 toVec3(const gp_Pnt &p) {
+    return glm::dvec3(p.X(), p.Y(), p.Z());
 }
 
-inline gp_Pnt toPnt(const glm::vec3 &p) {
-    return gp_Pnt(double(p.x), double(p.y), double(p.z));
+inline gp_Pnt toPnt(const glm::dvec3 &p) {
+    return gp_Pnt(p.x, p.y, p.z);
 }
 
 } // namespace shapecraft
